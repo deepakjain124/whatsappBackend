8171cf7bacfdcbf48a8cf681d315fcb4fb53139e
deepakjai@165 <deepak@ezulix.com>
2023-07-26 14:27:43 +0530

    deepak jain changes new

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const path = require("path");
 const { initializeSocket } = require("./Socket/socket");
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
+const port=process.env.PORT || 5000
 
 const whitelist = ["http://localhost:3001"];
 app.use(express.json());
-
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -38,7 +38,7 @@ app.options("*",cors({
 }))
 app.use("/api/v1/user", userRoutes);
 
-const server = app.listen(5000, () => {
+const server = app.listen(port, () => {
   console.log("Your Server listening on port:", 5000);
 });
 initializeSocket(server);
