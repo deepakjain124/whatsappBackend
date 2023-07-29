4c98074b817471c69864445bde017d73badf37f8
deepakjai@165 <deepak@ezulix.com>
2023-07-29 10:33:05 +0530

    deepak jain changes new try 28 july

--- Config/db.js
+++ Config/db.js
@@ -13,3 +13,19 @@ function mongoConnect(){
 
 
 module.exports = mongoConnect;
+
+// const mongoose = require('mongoose');
+// function mongoConnect(){
+//     mongoose.set("strictQuery", false);
+//     mongoose.connect("mongodb://0.0.0.0:27017/whatsappclone", {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }).then(()=>{
+//             console.log('Connected to MongoDB');
+//     }).catch(err => {
+//             console.error('Error connecting to MongoDB', err);
+//     })
+// }
+
+
+// module.exports = mongoConnect;
--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const fileUpload=require("express-fileupload")
+const fileUpload = require("express-fileupload");
 const mongoConnect = require("./Config/db");
 const userRoutes = require("./Router/userRouter");
 const path = require("path");
 const { initializeSocket } = require("./Socket/socket");
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
-const port=process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const whitelist = ["http://localhost:3001"];
 app.use(express.json());
@@ -26,9 +26,11 @@ const corsOptions = {
   },
   credentials: true,
 };
-app.use(fileUpload({
-  useTempFiles:true
-}))
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 app.use(
   cors({
     origin: "https://latestwhatsapp.netlify.app",
@@ -36,7 +38,6 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
-
 app.use("/api/v1/user", userRoutes);
 
 const server = app.listen(port, () => {
